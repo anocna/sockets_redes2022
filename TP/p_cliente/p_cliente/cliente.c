26cb0535acc66ce54927a94716556f531992f8f9
anocna <62865821+anocna@users.noreply.github.com>
2022-11-07 18:36:35 -0300

    Add files via upload

--- TP/p_cliente/p_cliente/cliente.c
+++ TP/p_cliente/p_cliente/cliente.c
@@ -2,39 +2,33 @@
 /*********************PROGRAMA CLIENTE*********************/
 /***********************************************************/
 
-// Estructura utilizada por el socket
-/*
-struct sockaddr_in{
-	short int sin_family; 			// Familia de direcciones. Usaremos siempre "AF_INET"
-	unsigned short int sin_port; 	// Numero de puerto
-	struct in_addr sin_addr;		// Estructura que indica la direccion IP
-	unsigned char sin_zero[8];		// Array de 8 bytes rellenados a cero.
-}
-*/
- 
-
-// Protocolos disponibles para el transporte de datos
-/*
-TCP (SOCK_STREAM) --> 	Socket de flujo que define una comunicacion bidireccional, confiables y orientada a la conexion.
-						Todo lo que se mande desde el origen llegarï¿½ al destino.
-						Necesitan mantener una conexion abierta.
-
-UDP (SOCK_DGRAM) --> 	Socket de datagramas que no esta orientado a la conexion y no es confiable.
-						No precisan mantener una conexion abierta.
-*/
 
 #include <stdio.h>
 #include <winsock2.h>
 #include <string.h>
+#include <ctype.h>
 
+void op_uno();
+void op_dos();
+
+SOCKET skt;
+WSADATA wsa;
+int puerto=0, recv_size, aux=0, inicio=1;
+char mensaje[]="",ip[20]="",caux;
+
+	
 int main(int argc, char **argv){
-	WSADATA wsa;
-	SOCKET skt;
-	int puerto = 55228, recv_size, aux=1;
-	char mensaje[2000];
+	
+	
 	struct sockaddr_in server;
-
-	/**********************INICIALIZACION WINSOCK**********************/
+	char str_ip;
+	
+	do{
+	
+	printf("\n*********************************************************\n"
+			 "********************  BIENVENIDOS ***********************\n"
+			 "*********************************************************\n\n");
+	/**********************INICIALIZACION WINSOCK**********************/	
 	printf("Inicializando Winsock...");
 	if(WSAStartup(MAKEWORD(2, 2), &wsa) != 0){
 		printf("Error al inicializar winsock\n");
@@ -50,60 +44,198 @@ int main(int argc, char **argv){
 	}
 	printf("Socket creado\n");
 
-	/**********************CONFIGURACION SOCKADDR_IN**********************/
+	printf("Ingrese Direccion de IP: ");
+	fflush(stdin);
+	gets(mensaje);
+	
+	printf("Ingrese Puerto: ");
+	scanf("%d",&puerto);
+		
+	/********************** CONFIGURACION SOCKADDR_IN **********************/
 	server.sin_family = AF_INET; // Familia TCP/IP
 	server.sin_port = htons(puerto); // Puerto de la aplicacion del servidor
-	server.sin_addr.s_addr = inet_addr("127.0.0.1"); // Direccion IP del servidor
+	server.sin_addr.s_addr = inet_addr(mensaje); // Direccion IP del servidor "127.0.0.1"
+	
 
 	/**********************CONEXION AL SERVIDOR**********************/
 	if(connect(skt, (struct sockaddr *)&server, sizeof(server)) < 0){
-		printf("Error de conexion\n");
+		printf("\n!!! Error de conexion\n");
 		exit(-1);
 	}
 	printf("Conexion exitosa\n");
 
 	do{
-	/**********************ENVIAR UN MENSAJE**********************/
-	printf("Ingrese mensaje a enviar: "); gets(mensaje);
-	if(send(skt, mensaje, strlen(mensaje), 0) < 0){
-		printf("Error al enviar mensaje\n");
-		exit(-1);
-	}
-	
-	printf("Mensaje enviado exitosamente\n");
-
-	/**********************RECEPCION DE MENSAJE**********************/
-	printf("Esperando respuesta entrante...\n");
-	if((recv_size = recv(skt, mensaje, 2000, 0)) == SOCKET_ERROR)
-		printf("Recepcion fallida\n");
-
-	mensaje[recv_size] = '\0';
-	printf("Resultado: %s\n\n", mensaje);
-	
-	
-	/******* PREGUNTA ***********/
-	
-	printf("Desea realizar otra operacion? si/no\n-->"); gets(mensaje);
-			            
-	
-	if(send(skt, mensaje, strlen(mensaje), 0) < 0){
-		printf("Error al enviar mensaje\n");
-		exit(-1);
-	}
-	
-	printf("Mensaje enviado exitosamente\n");
-	mensaje[recv_size] = '\0';
-
-		if(strcmp(mensaje,"no")==0){
-			aux=0;
-			printf("Conexión Cerrada por el Cliente.\n");
+		system("cls");
 		
+	    printf(   "-------------------------------------"
+				"\n-*********  BIENVENIDOS ************-"
+				"\n-------------------------------------"
+				"\n   1. REALIZAR CALCULO."
+                "\n   2. VER REGISTRO DE ACTIVIDADES."
+                "\n   3. CERRAR SESION."
+                "\n-------------------------------------\n");
+
+        /* Filtramos la opción elegida por el usuario */
+        do
+        {
+            printf( "\n   Introduzca opcion (1-3): ");
+            scanf( "%d", &aux );
+            if(aux < 1 || aux > 3)
+                printf( "\n   Opcion Invalida \n");
+
+        } while ( aux < 1 || aux > 3 );
+        /* La opción sólo puede ser 1, 2, 3 */
+
+        switch ( aux )
+        {
+            case 1: op_uno();
+                    break;
+
+            case 2: op_dos();
+                    break;
+            
+            default: break;
+         }
+    } while ( aux != 3 );	
+			
+	strcpy(mensaje,"cerrar");
+	
+	if(send(skt, mensaje, strlen(mensaje), 0) < 0){
+		printf("	Error al enviar mensaje\n");
+		exit(-1);
 	}
 	
-	}while(aux==1);
+	printf("Mensaje de cierre enviado exitosamente\n");
 
 	closesocket(skt);
 	WSACleanup();
+	
+	system("cls");
+	
+	}while(inicio);
 
 	return 0;
 }
+
+
+
+void op_uno(){
+    
+    
+    
+    if(send(skt, "calculo", sizeof("calculo"), 0) < 0){
+		printf("	Error al enviar mensaje\n");
+		exit(-1);
+	}
+	
+	
+    
+    /**************** Validacion de cantidad de caracteres *****/
+    int largo=0,i=0;
+    
+    do{
+    	system("cls");
+    	
+        printf( "\n-->   Ingrese la operacion a realizar.\n\n"
+				  "-->   Ingrese ''volver'' para ir al menu principal\n\n"
+				  "-->	  ");
+        fflush(stdin);
+        gets(mensaje);
+        
+        printf("\n------------  %s\n", mensaje);
+        
+        largo=strlen(mensaje);
+        
+        for(i;i<=largo;i++)
+            mensaje[i] = tolower(mensaje[i]);
+        
+        if(strcmp(mensaje,"volver")==0)
+            return;
+        
+        
+        if(largo<2){
+        	system("cls");
+            printf( "\n   !!! ERROR, ingrese mas de dos caracteres.\n\n");
+            system("pause");
+    	}
+        if(largo>20){
+        	system("cls");
+            printf( "\n   !!! ERROR, ha ingresado demasiados caracteres.\n\n");
+            system("pause");
+    	}
+            
+    }while(largo<2 || largo>20);
+    
+    
+    /**********************ENVIAR EL MENSAJE*********************/
+	if(send(skt, mensaje, largo, 0) < 0){
+		printf("	Error al enviar mensaje\n");
+		exit(-1);
+	}
+	
+    printf( "\n	La operacion %s fue enviada al servidor.\n", mensaje);
+    
+    
+    /**********************RECEPCION DE MENSAJE*********************/
+	printf("	Esperando respuesta entrante...\n");
+	if((recv_size = recv(skt, mensaje, 200, 0)) == SOCKET_ERROR)
+		printf("	Recepcion fallida\n");
+
+	mensaje[recv_size] = '\0';	
+
+	/*if(!strcmp(mensaje,"inactivo")){
+			
+		printf("Cliente desconectado por inactividad\n");
+		aux=3;
+		
+	}else{*/
+		printf("	Resultado: %s\n\n", mensaje);
+	//}
+	
+	system("pause");
+	
+}
+
+
+void op_dos(){
+    
+    
+    system("cls");
+    	    
+    /**********************ENVIAR UN MENSAJE*********************/
+    if(send(skt, "logs", strlen(mensaje), 0) < 0){
+		printf("	Error al enviar mensaje\n");
+		exit(-1);
+	}
+	
+	printf("	Mensaje de log enviado exitosamente\n");
+   	
+    int auxfin = 0;
+		
+		
+    	while(!auxfin){
+    		
+    		/**********************RECEPCION DE MENSAJE*********************/
+	    	//printf("Esperando respuesta entrante...\n");
+	    	
+	    	if((recv_size = recv(skt, mensaje, 200, 0)) == SOCKET_ERROR)
+	    		printf("Recepcion fallida\n");
+	    		
+	    	/*if(!strcmp(mensaje,"inactivo")){
+					
+						printf("\nentro a estar inactivoooooo \n--> %s\n\n", mensaje); // verrrrrrrrrrrrrr
+						aux=3;
+					
+			}else{*/
+			
+    			if(!strcmp(mensaje,"FEOF"))
+					auxfin=1;
+				
+				printf("%s", mensaje);
+			//}
+		}
+		printf(".\n");
+		system("pause");
+    		
+	}
+
